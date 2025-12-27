dfc640e8ee52bc86e70d0d76db37a2f564cc637c
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-27 07:39:49 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -3,4 +3,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 const PORT = process.env.PORT;
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+})
