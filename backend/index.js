a4287b273e5807c666c254dce09a715798200aa0
WebDevloperVishal <vishalsanam2024@gmail.com>
2025-12-27 07:53:04 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,14 @@ const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+// app.listen(PORT, ()=>{
+//     console.log(`server is running on port ${PORT}`)
+// })
 
-// try {
-//     app.listen(PORT, () => {
-//         console.log(`Server is running on port ${PORT}`);
-//     });
-// } catch (error) {
-//     console.error('Error starting server:', error);
-// }
+try {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+} catch (error) {
+    console.error('Error starting server:', error);
+}
